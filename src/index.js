a509d4048a48257fc7ca1c3f3753ad9ae5251158
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-30 14:43:19 +0430

    get data from firebase database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello from index.js");
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArwu8aulrjz_rgzsaulvv2pU81IZZK2rY",
+  authDomain: "fir-9-mh.firebaseapp.com",
+  projectId: "fir-9-mh",
+  storageBucket: "fir-9-mh.appspot.com",
+  messagingSenderId: "809118794810",
+  appId: "1:809118794810:web:6da5e5fade713e19aca103",
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
